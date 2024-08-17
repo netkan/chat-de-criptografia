9f72cbf0d211e1201bfe0c5e2d68a5ba5267aecf
Karla Taina <karlataina0@gmail.com>
2024-08-17 18:37:41 -0300

    Update - add textarea and remove a p

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-let resultado = ""
-let texto = ""
+let resultado = "";
+let texto = "";
 
 function exibirTextonaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 } 
-
 function limpar2(){
-    exibirTextonaTela('.espaco-resultado', "");
+    document.getElementById("imagem-texto").style.display = "block";
+    document.getElementById("espaco-resultado").style.display = "none";
 }
-
+function trocaDeVisibilidade(){
+    document.getElementById("imagem-texto").style.display = "none";
+    document.getElementById("espaco-resultado").style.display = "block";
+}
 function codificar(){
-    exibirTextonaTela('.imagem', "")
-    texto = document.querySelector('textarea').value
+    texto = document.querySelector('.texto-inicial').value
     resultado = texto
-
     .replace(/a/g,"ai") 
     .replace(/e/g,"enter")
     .replace(/i/g,"imes")
     .replace(/o/g,"ober")
     .replace(/u/g,"ufat");
-    exibirTextonaTela('.espaco-resultado', resultado);
+    trocaDeVisibilidade()
+    exibirTextonaTela('#campo-resultado', resultado);
 }
-
 function decodificar(){
-    exibirTextonaTela('.imagem', "")
-    texto = document.querySelector('textarea').value
+    texto = document.querySelector('.texto-inicial').value
     resultado = texto
     .replace(/ufat/g,"u")
     .replace(/ober/g,"o")
     .replace(/imes/g,"i")
     .replace(/enter/g,"e")
     .replace(/ai/g,"a")
-    exibirTextonaTela('.espaco-resultado', resultado);
+    trocaDeVisibilidade()
+    exibirTextonaTela('#campo-resultado', resultado);
 }
-
