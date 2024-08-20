378cfe933797913f9ba95294163f239971c531a6
Karla Taina <karlataina0@gmail.com>
2024-08-19 22:54:47 -0300

    add botao de copiar

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let resultado = "";
 let texto = "";
+let mensagem = document.getElementById('campo-resultado');
+let botaoCopiar = document.getElementById('copiar');
 
 function exibirTextonaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -14,7 +16,7 @@ function trocaDeVisibilidade(){
     document.getElementById("espaco-resultado").style.display = "block";
 }
 function codificar(){
-    texto = document.querySelector('.texto-inicial').value
+    texto = document.querySelector('.texto-1').value
     resultado = texto
     .replace(/a/g,"ai") 
     .replace(/e/g,"enter")
@@ -25,7 +27,7 @@ function codificar(){
     exibirTextonaTela('#campo-resultado', resultado);
 }
 function decodificar(){
-    texto = document.querySelector('.texto-inicial').value
+    texto = document.querySelector('.texto-1').value
     resultado = texto
     .replace(/ufat/g,"u")
     .replace(/ober/g,"o")
@@ -35,3 +37,8 @@ function decodificar(){
     trocaDeVisibilidade()
     exibirTextonaTela('#campo-resultado', resultado);
 }
+botaoCopiar.addEventListener("click", function() {
+        mensagem.select(); 
+        mensagem.setSelectionRange(0, 99999); 
+        navigator.clipboard.writeText(mensagem.value);
+})
